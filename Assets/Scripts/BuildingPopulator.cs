b78993bb97b4c0d2d58dfa810dc68cfa4f60af15
Quinn <Quinn.mcneil@tidygames.co.uk>
2023-05-11 16:11:23 +0100

    added proper building placement

--- Assets/Scripts/BuildingPopulator.cs
+++ Assets/Scripts/BuildingPopulator.cs
@@ -14,6 +14,8 @@ public class BuildingPopulator : Singleton<BuildingPopulator>
     public Vector2Int gridSize;
     public Vector3 spaceSize;
     public Vector3 startPoint;
+    [Space]
+    public MeshBuilder mb;
 
     private GameObject[][] buildings;
     private LayerMask layerMask;
@@ -35,9 +37,41 @@ public class BuildingPopulator : Singleton<BuildingPopulator>
     {
         if (spawnBuildings)
         {
+            Vector3[] bounds = FindLowestPoint();
+
+            startPoint = bounds[0] - (spaceSize * 3);
+            startPoint.y = bounds[0].y;
+
+            Vector3 gridSizeFloat = ((bounds[1] + (spaceSize*3)) - startPoint);
+            gridSize.x = Mathf.CeilToInt(gridSizeFloat.x / spaceSize.x);
+            gridSize.y = Mathf.CeilToInt(gridSizeFloat.z / spaceSize.z);
+
             StartCoroutine(PlaceBuildings());
             spawnBuildings = false;
         }
+    }
+
+    private Vector3[] FindLowestPoint()
+    {
+        Vector3 lowestPoint = Vector3.positiveInfinity;
+        Vector3 highestPoint = Vector3.negativeInfinity;
+
+        foreach (Mesh mesh in mb.meshes)
+        {
+            foreach (Vector3 vertex in mesh.vertices)
+            {
+                if (vertex.x < lowestPoint.x) lowestPoint.x = vertex.x;
+                if (vertex.z < lowestPoint.z) lowestPoint.z = vertex.z;
+
+                if (vertex.x > highestPoint.x) highestPoint.x = vertex.x;
+                if (vertex.z > highestPoint.z) highestPoint.z = vertex.z;
+
+            }
+        }
+
+        lowestPoint.y = highestPoint.y = startPoint.y;
+
+        return new Vector3[] { lowestPoint, highestPoint };
     }
 
     IEnumerator PlaceBuildings() 
@@ -55,7 +89,7 @@ public class BuildingPopulator : Singleton<BuildingPopulator>
                 position.x += x * spaceSize.x + (spaceSize.x / 2);
                 position.z += y * spaceSize.z + (spaceSize.z / 2);
 
-                if (! Physics.CheckBox(position, spaceSize * 0.6f, Quaternion.identity, layerMask))
+                if (! Physics.CheckBox(position, spaceSize * 0.4f, Quaternion.identity, layerMask))
                 {
                     buildings[x][y] = Instantiate(RandomPrefab(), position, Quaternion.identity, transform);
                 }
